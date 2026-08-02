5dbc5a4d697984ca3b48bdf5e8772d55c4c158b0
kunal <kunalk8728@gmail.com>
2026-08-02 22:16:36 +0530

    adding the gsap

--- src/pages/Agence.jsx
+++ src/pages/Agence.jsx
@@ -1,24 +1,43 @@
-import React from 'react'
-
+import React, { useRef } from 'react'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
 const Agence = () => {
+  gsap.registerPlugin(ScrollTrigger)
+  const imageDivRef = useRef(null)
+
+  useGSAP(function () {
+    gsap.to(imageDivRef.current, {
+      scrollTrigger: {
+        trigger: imageDivRef.current,
+        markers: true,
+        start: 'top 80%',
+        end: 'top 30%',
+        scrub:true,
+        markers:true
+      }
+    })
+  })
+
+
   return (
-   <div>
-     <div className='section1'>
-      <div className='w-[15vw] h-[20vw] rounded-4xl overflow-hidden absolute top-40 left-[30vw] '>
-        <img className='h-full object-cover w-full ' src="https://images.unsplash.com/photo-1520697830682-bbb6e85e2b0b?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+    <div>
+      <div className='section1'>
+        <div ref={imageDivRef} className='w-[15vw] h-[20vw] rounded-4xl overflow-hidden absolute top-40 left-[30vw] '>
+          <img className='h-full object-cover w-full ' src="https://images.unsplash.com/photo-1520697830682-bbb6e85e2b0b?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+        </div>
+        <div className='font-[font2] relative'>
+          <div className='mt-[55vh]'>
+            <h1 className='text-[19vw] text-center leading-[17vw]'>sparrow <br /> lite</h1>
+          </div>
+          <div className='pl-[50%] bg-red-500'>
+            <p className='text-6xl'>
+              &nbsp;&nbsp; &nbsp;&nbsp;  We think in threats, patterns and consequences — not just features. A system built for defense and security has to earn trust before it earns adoption. If we ignore that, we can ship something that looks good in a demo, but not something that survives contact with the real world. We bring that discipline to every build.</p>
+          </div>
+        </div>
       </div>
-      <div className='font-[font2] relative'>
-      <div className='mt-[55vh]'>
-        <h1 className='text-[19vw] text-center leading-[17vw]'>sparrow <br /> lite</h1>
-      </div>
-      <div className='pl-[50%] bg-red-500'>
-        <p className='text-6xl'> 
-        &nbsp;&nbsp; &nbsp;&nbsp;  We think in threats, patterns and consequences — not just features. A system built for defense and security has to earn trust before it earns adoption. If we ignore that, we can ship something that looks good in a demo, but not something that survives contact with the real world. We bring that discipline to every build.</p>
-      </div>
+      <div className='section2 h-screen'></div>
     </div>
-    </div>
-    <div className='section2 h-screen'></div>
-   </div>
   )
 }
 
