c21e60e61cf87c0f739d5cdae1a057c56c684ddf
kunal <kunalk8728@gmail.com>
2026-08-02 14:26:03 +0530

    fix the bugs in link

--- src/components/home/HomeBottomText.jsx
+++ src/components/home/HomeBottomText.jsx
@@ -4,12 +4,17 @@ import { Link, Links } from 'react-router-dom'
 const HomeBottomText = () => {
   return (
     <div className='font-[text-1] flex justify-center items-center gap-6  text-white'>
-      <Link className='text-[7vw] border-4 border-white rounded-full uppercase leading-[6vw] px-9 pt-1.5 '>
+     <div className=' border-4 border-white rounded-full uppercase leading-[2vw] px-9 pt-8 hover:border-green-300 hover:text-green-400'>
+       <Link className='text-[7vw] mt-6 ' to ="/Projects">
         deploy
       </Link>
-      <Link className='text-[7vw] border-4 border-white rounded-full px-9 leading-[6vw] uppercase pt-1.5 '>
+     </div>
+     <div className=' border-4 border-white rounded-full px-9 leading-[2vw] uppercase pt-8 hover:border-green-300 hover:text-green-400'>
+ <Link className=' text-[7vw] mt-6' to="/Agence">
         Agence
       </Link>
+     </div>
+     
     </div>
   )
 }
--- src/components/home/HomeHeroText.jsx
+++ src/components/home/HomeHeroText.jsx
@@ -3,9 +3,9 @@ import Video from './Video'
 
 const HomeHeroText = () => {
   return (
-    <div className=' font-[text-1] text-white pt-11 text-center'>
+    <div className=' font-[text-1] text-white pt-2 text-center'>
       <div className='text-[9vw] leading-[8vw] flex items-center justify-center'>
-        BUILT FOR THE
+        BUILD FOR THE
       </div>
       <div className='text-[9vw] leading-[8vw] flex flex-wrap  items-center justify-center'>
         FUTURE
