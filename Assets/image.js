e2d3db51d343a346d5a5c0c156d636637907791a
kunal <kunalk8728@gmail.com>
2026-08-03 23:13:33 +0530

    putting all the images in image.js

---
+++ Assets/image.js
@@ -0,0 +1,9 @@
+const ImagesArray = [
+    " https://imgs.search.brave.com/3U1Dqt3qZeYfd0LcD4CP68pDtsDggdMxrxACeGY4jEY/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9wcmV2/aWV3LnJlZGQuaXQv/aG93LWlzLWl0LWxp/dmluZy1pbi10aGUt/bm9ydGhlYXN0ZXJu/LWNvcm5lci1vZi1p/bmRpYS12MC12eWQx/Ynd1dXlpZmcxLmpw/ZWc_d2lkdGg9NDI4/NCZmb3JtYXQ9cGpw/ZyZhdXRvPXdlYnAm/cz1kYTQ4MTQwNTRm/Yjk1MzQ5MTU1Mjkx/MDNkZDAzZDNiYjg5/YTkwYTY4",
+    "https://preview.redd.it/can-anyone-from-ne-share-their-best-photo-of-landscape-of-v0-sh6tselok87f1.jpeg?width=4000&format=pjpg&auto=webp&s=3b26faeaf1cac61a7354922ae2dd11c72b7fad70",
+    "https://static.vecteezy.com/system/resources/thumbnails/022/393/717/small/total-panorama-view-of-zig-zag-road-in-old-silk-route-east-sikkim-photo.JPG",
+    "https://i.pinimg.com/originals/99/c4/4c/99c44cffc29c444d07a35f9909e7fbec.jpg",
+    "https://i.pinimg.com/originals/5e/cb/e6/5ecbe6297b3d441da8a1e4bc9fa46f5e.jpg"
+]
+
+export default ImagesArray
