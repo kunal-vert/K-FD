534b76fbed18fa394e772e6a00558014a42a1351
kunal <kunalk8728@gmail.com>
2026-08-12 12:30:15 +0530

    reduce the top padding

--- src/components/home/HomeBottomText.jsx
+++ src/components/home/HomeBottomText.jsx
@@ -5,12 +5,12 @@ const HomeBottomText = () => {
   return (
     <div className='font-[text-1] flex justify-center items-center gap-6  text-white'>
      <div className=' border-4 border-white rounded-full uppercase leading-[2vw] px-9 pt-8 hover:border-green-300 hover:text-green-400'>
-       <Link className='text-[7vw] mt-6 ' to ="/Projects">
+       <Link className='text-[7vw] mt-1 ' to ="/Projects">
         deploy
       </Link>
      </div>
      <div className=' border-4 border-white rounded-full px-9 leading-[2vw] uppercase pt-8 hover:border-green-300 hover:text-green-400'>
- <Link className=' text-[7vw] mt-6' to="/Agence">
+ <Link className=' text-[7vw] mt-1' to="/Agence">
         Agence
       </Link>
      </div>
