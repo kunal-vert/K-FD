e056c407a0d28f598e20afa2d2a03a063e33eedb
kunal <kunalk8728@gmail.com>
2026-08-13 22:45:29 +0530

    added the imageArray fxn

--- src/pages/Agence.jsx
+++ src/pages/Agence.jsx
@@ -1,8 +1,18 @@
 import React, { useRef } from 'react'
-import ImagesArray from '../../Assets/image'
 import { useGSAP } from '@gsap/react'
 import gsap from 'gsap'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
+
+const ImagesArray = [
+  '/pic/pic-1.png',
+  '/pic/pic-2.png',
+  '/pic/pic-3.png',
+  '/pic/pic-4.png',
+  '/pic/pic-5.png',
+  '/pic/pic-6.png',
+  '/pic/pic-7.png',
+];
+
 const Agence = () => {
   gsap.registerPlugin(ScrollTrigger)
   const imageDivRef = useRef(null)
@@ -10,21 +20,20 @@ const Agence = () => {
 
 
 
- 
 
 
   return (
     <div>
       <div className='section1'>
-        <div ref={imageDivRef} className='w-[15vw] h-[20vw] rounded-4xl overflow-hidden absolute top-56 left-[30vw] '>
-          <img ref={imageref} className='h-full object-cover w-full ' src="https://images.unsplash.com/photo-1520697830682-bbb6e85e2b0b?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+        <div ref={imageDivRef} className='w-[18vw] h-[23vw] rounded-2xl overflow-hidden absolute top-56 left-[30vw] '>
+          <img ref={imageref} className='h-full object-cover w-full ' src="https://www.orfonline.org/public/uploads/posts/image/65d5a5d6b48d4.png" alt="" />
         </div>
         <div className='font-[font2] relative'>
           <div className='mt-[55vh]'>
             <h1 className='text-[19vw] text-center leading-[17vw]'>sparrow <br /> lite</h1>
           </div>
           <div className='pl-[50%] '>
-            <p className='text-6xl'>
+            <p className='text-2xl text-white'>
               &nbsp;&nbsp; &nbsp;&nbsp;  We think in threats, patterns and consequences — not just features. A system built for defense and security has to earn trust before it earns adoption. If we ignore that, we can ship something that looks good in a demo, but not something that survives contact with the real world. We bring that discipline to every build.</p>
           </div>
         </div>
